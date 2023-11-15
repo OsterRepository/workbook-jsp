9da083b6945ad27200fed55294a2a09be1c0a238
oster <kms0843@naver.com>
2023-11-15 22:14:02 +0900

    쿠키를 이용해 자동 로그인 기능 UUID 생성하여 DB에 등록 하고 쿠키를 발행하여 데이터를 담아 자동로그인 가능하게 구현

--- src/main/java/org/zerock/w2/filter/LoginCheckFilter.java
+++ src/main/java/org/zerock/w2/filter/LoginCheckFilter.java
@@ -2,13 +2,15 @@ package org.zerock.w2.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.*;
 import lombok.extern.log4j.Log4j2;
+import org.zerock.w2.dto.MemberDTO;
+import org.zerock.w2.service.MemberService;
 
 import java.io.IOException;
+import java.rmi.server.ExportException;
+import java.util.Arrays;
+import java.util.Optional;
 
 @WebFilter(urlPatterns = {"/todo/*"})
 @Log4j2
@@ -23,12 +25,55 @@ public class LoginCheckFilter implements Filter {
 
         HttpSession session = req.getSession();
 
-        if(session.getAttribute("loginInfo")==null){
+        if(session.getAttribute("loginInfo") != null){
+            chain.doFilter(request,response);
+            return;
+        }
+
+        //session에 loginInfo 값이 없다면 쿠키를 체크
+        Cookie cookie = findCookie(req.getCookies(), "remember-me");
+
+        //session에도 없고 쿠키도 없다면 그냥 로그인으로
+        if(cookie == null){
             resp.sendRedirect("/login");
             return;
         }
 
+        //쿠키가 존재하는 상황
+        log.info("cookie는 존재하는 상황");
+        //uuid값
+        String uuid = cookie.getValue();
+
+        try {
+            //데이터베이스 확인
+            MemberDTO memberDTO = MemberService.INSTANCE.getByUUID(uuid);
+
+            log.info("쿠키의 값으로 조회한 사용자 정보 : "+ memberDTO);
+            if(memberDTO == null){
+                throw new Exception("Cookie value is not valid");
+            }
+            //회원 정보를 세션에 추가
+            session.setAttribute("loginInfo", memberDTO);
+            chain.doFilter(request,response);
+        }catch (Exception e){
+            e.printStackTrace();
+            resp.sendRedirect("/login");
+        }
+
 
         chain.doFilter(request,response);
+    }
+
+    private Cookie findCookie(Cookie[] cookies, String name){
+
+        if(cookies == null || cookies.length==0){
+            return null;
+        }
+
+        Optional<Cookie> result = Arrays.stream(cookies)
+                .filter(ck -> ck.getName().equals(name))
+                .findFirst();
+
+        return result.isPresent()?result.get():null;
     }
 }
